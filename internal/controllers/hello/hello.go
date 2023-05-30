c3edc4d3b856722cd8055d351281f2f2de38d438
vladimir.butakov <vbutakov2020@gmail.com>
2023-05-30 22:26:14 +0300

    bxb_home_1 HTTP Сервер с middleware

--- internal/controllers/hello/hello.go
+++ internal/controllers/hello/hello.go
@@ -8,6 +8,4 @@ import (
 func Handler(w http.ResponseWriter, request *http.Request) {
 	name := request.URL.Query().Get("name")
 	fmt.Fprintf(w, `{"message":"Hello, %s!"}`, name)
-	//logger := loggerServer.New()
-	//logger.Log(request)
 }
--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -6,19 +6,6 @@ import (
 	"time"
 )
 
-//type Logger struct {
-//}
-
-//func New() *Logger {
-//	return &Logger{}
-//}
-//
-//func (l Logger) Log(request *http.Request) {
-//	start := time.Now()
-//	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-//	infoLog.Printf("%s %s %s", request.Method, request.RequestURI, time.Since(start))
-//}
-
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		start := time.Now()
