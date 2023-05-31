5dc14f45f1e2995f490973a90d892f9264fd3947
vladimir.butakov <vbutakov2020@gmail.com>
2023-05-31 17:45:07 +0300

    bxb_home_1 HTTP Сервер с middleware

--- main.go
+++ main.go
@@ -5,12 +5,4 @@ import HelloController "bxb_home/internal/server/http"
 func main() {
 	hello := HelloController.New()
 	hello.Start()
-	//addr := os.Getenv("ADDR")
-	//
-	//mux := http.NewServeMux()
-	//helloController := Server.New()
-	//mux.HandleFunc("/v1/hello", helloController.HelloHandler)
-	//
-	//log.Printf("server is listening at %s", addr)
-	//log.Fatal(http.ListenAndServe(":3000", mux))
 }
