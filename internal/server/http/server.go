85da88f9a3f305100935b6317d8e539e43df5391
vladimir.butakov <vbutakov2020@gmail.com>
2023-06-04 13:46:51 +0300

    bxb_home_1 HTTP Сервер с middleware

--- internal/server/http/server.go
+++ internal/server/http/server.go
@@ -3,25 +3,38 @@ package http
 import (
 	"bxb_home/internal/controllers/hello"
 	"bxb_home/internal/middleware"
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
 type Server struct {
-	server http.Handler
+	handler http.Handler
+	server  *http.Server
 }
 
 func New() *Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/v1/hello", hello.Handler)
 	handler := middleware.Logging(mux)
+	server := &http.Server{Addr: ":3000", Handler: handler}
 
-	return &Server{server: handler}
+	return &Server{handler: handler, server: server}
 }
 
 func (h Server) Start() {
 	addr := os.Getenv("ADDR")
-	log.Printf("server is listening at %s", addr)
-	log.Fatal(http.ListenAndServe(":3000", h.server))
+	log.Printf("handler is listening at %s", addr)
+	log.Fatal(h.server.ListenAndServe())
+}
+
+func (h *Server) Stop(ctx context.Context) error {
+	log.Println("stopping server...")
+	err := h.server.Shutdown(ctx)
+	if err != nil {
+		return fmt.Errorf("handler shutdown: %w", err)
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import HelloController "bxb_home/internal/server/http"
+import (
+	HelloController "bxb_home/internal/server/http"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
 	hello := HelloController.New()
+
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+		<-sigChan
+
+		shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+		defer shutdownRelease()
+
+		log.Fatal(hello.Stop(shutdownCtx))
+	}()
+
 	hello.Start()
 }
